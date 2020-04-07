d29fa429d3f074bf07b1cb7fab35ade9b4b0ce85
Izalia-b <crazy_chemist7@mail.ru>
2020-04-07 20:38:01 +0300

    block 3 - 8

--- src/App.js
+++ src/App.js
@@ -10,44 +10,60 @@ class App extends Component{
       {name:'Mazda',year : 2018},
       {name:'Audi',year : 2016}
     ],
-    pageTitel:"React component"
+    pageTitel:"React component",
+    showCars : false,
   }
-changeTitelHandler =(newTitel)=>{
- this.setState({
-   pageTitel: newTitel
- })
-}
+  onChangeName (name,index){
+    const car = this.state.cars[index] //получили машину на которую нажали инпут
+    car.name = name //для нее установили имя из инпут
+    const cars = [... this.state.cars] //копия state
+    cars[index] = car // из копии взяли машину на которую нажали импут и прировняли к новой машине
+    this.setState ({cars}) // заменили
+  }
 
-handleInput =(event)=>{
+toogleCarsHandler =()=>{
   this.setState({
-    pageTitel: event.target.value
+    showCars : !this.state.showCars
   })
 }
+
+deleteHandle(index){
+  const cars = this.state.cars.concat()
+  cars.splice(index,1)
+  this.setState({cars})
+}
+
   render (){
   const divStyle ={
     textAlign: 'center'
   };
 
-   
+  let cars = null 
+  if (this.state.showCars){
+    cars =this.state.cars.map((car,index)=>{
+    return(
+      <Car 
+      key={index}
+      name = {car.name}
+      year = {car.year}
+      onChangeName = {(event)=>this.onChangeName(event.target.value,index)}
+      onDelete= {this.deleteHandle.bind(this,index)}
+      />
+    )
+  }) 
+}
+
   return (
     <div style= {divStyle}>
       <h1>{this.state.pageTitel}</h1>
-      <input type= 'text' onChange ={this.handleInput}/>
-      <button onClick ={this.changeTitelHandler.bind(this,'Changed')}>Change title</button>
-      {this.state.cars.map((car,index)=>{
-       return(
-         <Car 
-         key={index}
-         name = {car.name}
-         year = {car.year}
-         onChangeTitle = {()=>this.changeTitelHandler(car.name)}
-         />
-       )
-     })
-     }
+      
+      <button onClick ={this.toogleCarsHandler}>Show cars</button>
+      
+      {cars}  
+
     </div>
   );
-  }
+}
 }
 export default App;
 
