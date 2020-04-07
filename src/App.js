fcdcb52699c546f78da8c8900ea3557c312702cb
Izalia-b <crazy_chemist7@mail.ru>
2020-04-07 17:52:19 +0300

    block3-6

--- src/App.js
+++ src/App.js
@@ -28,24 +28,23 @@ handleInput =(event)=>{
     textAlign: 'center'
   };
 
-   const cars = this.state.cars
+   
   return (
     <div style= {divStyle}>
       <h1>{this.state.pageTitel}</h1>
       <input type= 'text' onChange ={this.handleInput}/>
       <button onClick ={this.changeTitelHandler.bind(this,'Changed')}>Change title</button>
-      <Car  
-      name={cars[0].name} 
-      year={cars[0].year}
-      onChangeTitle = {this.changeTitelHandler.bind(this,cars[0].name)}/>
-      <Car  
-      name={cars[1].name} 
-      year={cars[1].year}
-      onChangeTitle = {()=>this.changeTitelHandler(cars[1].name)}/>
-      <Car  
-      name={cars[2].name} 
-      year={cars[2].year}
-      onChangeTitle = {this.changeTitelHandler.bind(this,cars[2].name)}/>
+      {this.state.cars.map((car,index)=>{
+       return(
+         <Car 
+         key={index}
+         name = {car.name}
+         year = {car.year}
+         onChangeTitle = {()=>this.changeTitelHandler(car.name)}
+         />
+       )
+     })
+     }
     </div>
   );
   }
