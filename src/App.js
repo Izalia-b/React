3e0bafec60927b8ba1fea6bfba12dd0a0a1c5496
Izalia-b <crazy_chemist7@mail.ru>
2020-04-08 15:49:38 +0300

    block4-3

--- src/App.js
+++ src/App.js
@@ -16,54 +16,57 @@ class App extends Component{
   onChangeName (name,index){
     const car = this.state.cars[index] //получили машину на которую нажали инпут
     car.name = name //для нее установили имя из инпут
-    const cars = [... this.state.cars] //копия state
+    const cars = [...this.state.cars] //копия стейта
     cars[index] = car // из копии взяли машину на которую нажали импут и прировняли к новой машине
     this.setState ({cars}) // заменили
   }
 
-toogleCarsHandler =()=>{
+  toogleCarsHandler =()=>{
   this.setState({
     showCars : !this.state.showCars
   })
-}
+  }
 
-deleteHandle(index){
-  const cars = this.state.cars.concat()
-  cars.splice(index,1)
-  this.setState({cars})
-}
+  deleteHandle(index){
+    const cars = this.state.cars.concat()
+    cars.splice(index,1)
+    this.setState({cars})
+  }
 
   render (){
-  const divStyle ={
-    textAlign: 'center'
+    const divStyle ={
+      textAlign: 'center'
   };
 
-  let cars = null 
-  if (this.state.showCars){
-    cars =this.state.cars.map((car,index)=>{
-    return(
-      <Car 
-      key={index}
-      name = {car.name}
-      year = {car.year}
-      onChangeName = {(event)=>this.onChangeName(event.target.value,index)}
-      onDelete= {this.deleteHandle.bind(this,index)}
-      />
-    )
-  }) 
-}
+    let cars = null 
+    if (this.state.showCars){
+      cars =this.state.cars.map((car,index)=>{
+        return(
+          <Car 
+          key={index}
+          name = {car.name}
+          year = {car.year}
+          onChangeName = {(event)=>this.onChangeName(event.target.value,index)}
+          onDelete= {this.deleteHandle.bind(this,index)}
+          />
+        )
+      }) 
+    }
 
-  return (
-    <div style= {divStyle}>
-      <h1>{this.state.pageTitel}</h1>
-      
-      <button onClick ={this.toogleCarsHandler}>Show cars</button>
-      
-      {cars}  
-
-    </div>
-  );
-}
+    return (
+      <div style= {divStyle}>
+        <h1>{this.state.pageTitel}</h1>
+        <button onClick ={this.toogleCarsHandler}>Show cars</button>
+        <div style ={{
+          width: 400,
+          margin: 'auto',
+          paddingTop: 20
+        }}>
+          {cars}  
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
 
