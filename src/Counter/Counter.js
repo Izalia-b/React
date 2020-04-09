cdbfca6a44f80b35f2f25ae4caa35aeb9afed9cf
Izalia-b <crazy_chemist7@mail.ru>
2020-04-09 19:11:01 +0300

    block5-10

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import Auxiliary from '../hoc/Auxiliary';
 
 export default class Counter extends Component{
 
@@ -7,35 +8,35 @@ export default class Counter extends Component{
     }
 
     render(){
-    //     return(
-    //     <div>
-    //         <h2>Counter {this.state.counter}</h2>
-    //         <button onClick ={()=>{
-    //             this.setState({
-    //                 counter: this.state.counter+1
-    //             })
-    //         }}> + </button>
+        return(
+        <Auxiliary>
+            <h2>Counter {this.state.counter}</h2>
+            <button onClick ={()=>{
+                this.setState({
+                    counter: this.state.counter+1
+                })
+            }}> + </button>
 
-    //         <button onClick ={()=>{
-    //             this.setState({
-    //                 counter: this.state.counter-1
-    //             })
-    //         }}> - </button>
-    //     </div>
-    // )
-    return[
-        <h2 key="1">Counter {this.state.counter}</h2>,
-        <button  key="2" onClick ={()=>{
-            this.setState({
-                counter: this.state.counter+1
-            })
-        }}> + </button>,
+            <button onClick ={()=>{
+                this.setState({
+                    counter: this.state.counter-1
+                })
+            }}> - </button>
+        </Auxiliary>
+    )
+    // return[
+    //     <h2 key="1">Counter {this.state.counter}</h2>,
+    //     <button  key="2" onClick ={()=>{
+    //         this.setState({
+    //             counter: this.state.counter+1
+    //         })
+    //     }}> + </button>,
 
-        <button  key="3" onClick ={()=>{
-            this.setState({
-                counter: this.state.counter-1
-            })
-        }}> - </button>,
-    ]
+    //     <button  key="3" onClick ={()=>{
+    //         this.setState({
+    //             counter: this.state.counter-1
+    //         })
+    //     }}> - </button>,
+    // ]
     }
 }
