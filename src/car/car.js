69af725bc80c88585a5815a9dd4f80e5519de294
Izalia-b <crazy_chemist7@mail.ru>
2020-04-10 14:13:13 +0300

    block5-12

--- src/car/car.js
+++ src/car/car.js
@@ -1,38 +1,8 @@
 import React from 'react';
 import './car.css';
-// import Radium from 'radium' ;
+import  withClass from '../hoc/withClass';
 
 class Car extends React.Component{
-
-componentWillReceiveProps(nextProps){
-console.log('Car componentWillReceiveProps ',nextProps)
-}
-
-shouldComponentUpdate(nextProps,nextState){
-    console.log('Car shouldComponentUpdate ',nextProps,nextState)
-    return nextProps.name.trim() !== this.props.name.trim()
-}
-
-componentWillUpdate(nextProps,nextState){
-    console.log('Car componentWillUpdate ',nextProps,nextState)
-}
-
-componentDidUpdate(){
-    console.log('Car componentDidUpdate ')
-}
-
-// getSnapshotBeforeUpdate (){
-// console.log('Car getSnapshotBeforeUpdate')
-// }
-
-componentWillUnmount(){
-    console.log('Car componentWillUnmount ')
-}
-
-// static getDerivedStateFromProps(nextProps,prevState){
-//     console.log('Car getDerivedStateFromProps',nextProps)
-//     return prevState
-// }
 
     render(){
        
@@ -48,18 +18,10 @@ componentWillUnmount(){
         if(this.props.name.length > 4){
             inputClasses.push('bold')
         }
-         const style ={
-            boxShadow:'0 4px 5px 0 rgb(0,0,0,.14)',
-            border: '1px solid #ccc',
-            ':hover':{
-                boxShadow:'0 4px 5px 0 rgb(0,0,0,.25)',
-                border: '1px solid #aaa',
-            },
-            cursor: 'pointer',
-         }
+        
     
         return(
-            <div className = 'Car' style= {style}>
+            <React.Fragment>
                 <h3>Car name:{this.props.name} </h3>
                 <p> Year: <strong> {this.props.year}</strong></p>
                 <input 
@@ -68,10 +30,9 @@ componentWillUnmount(){
                 value ={this.props.name}
                 className ={ inputClasses.join(' ') } />
                 <button onClick = {this.props.onDelete}>Delete</button>
-            </div>
+            </React.Fragment>
         )
     }
 }
 
-//  export default Radium(Car)
-export default Car
+export default withClass(Car,'Car')
---
+++ src/hoc/withClass.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const withClass=(Component,className) => {
+    return (props)=>{
+        return (
+            <div className={className}>
+                <Component {...props}/>
+            </div>
+        )
+    }
+}
+export default withClass;
